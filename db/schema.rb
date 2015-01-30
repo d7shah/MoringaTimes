b4e914e43d9fa98583eac0776d09254aa24cc963
Dhruti Shah <dhrutishah10@gmail.com>
2015-01-30 11:52:20 +0300

    modified schema and added migration file

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150130075437) do
+ActiveRecord::Schema.define(version: 20150130084725) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "author"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "title"
   end
 
 end
