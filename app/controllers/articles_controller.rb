61a510b9a33f1a0f08dfc850c0afb71e30e90f71
Dhruti Shah <dhrutishah10@gmail.com>
2015-01-30 16:14:42 +0300

    fixed edit and update action view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,6 +22,18 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+  	@article = Article.find(params[:id])
+  end
+
+  def update
+  	@article = Article.find(params[:id])
+
+  	if @article.update(article_params)
+  		redirect_to @article
+  	else
+  		render 'edit'
+  	end
+
   end
 
   private
