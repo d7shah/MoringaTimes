23b2080b198c300ad13e146d6374c4d4fa53e781
Dhruti Shah <dhrutishah10@gmail.com>
2015-01-30 15:40:13 +0300

    Add create actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,9 +7,19 @@ class ArticlesController < ApplicationController
   	@article = Article.new
   end
 
+  def create
+  	@article = Article.new(params)
+
+  	if @article.save
+  		redirect_to @article
+  	else
+  		render 'new'
+  	end
+  end
+
   def show
   end
 
   def edit
   end
-end
+ end
